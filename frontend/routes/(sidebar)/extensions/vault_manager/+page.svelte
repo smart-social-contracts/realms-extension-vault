8cd4cb4e783c6af6d85983bc7e1ab1a9ec48e6b5
jmpmonteagudo <jmpmonteagudo@gmail.com>
2025-10-23 17:27:33 +0200

    refactor: rename vault extension folder to vault_manager for consistency

--- frontend/routes/(sidebar)/extensions/vault_manager/+page.svelte
+++ frontend/routes/(sidebar)/extensions/vault_manager/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import VaultManager from '$lib/extensions/vault_manager';
+	import VaultManager from '$lib/extensions/vault';
 	import { SITE_NAME } from '$lib/globals';
 	import MetaTag from '../../../utils/MetaTag.svelte';
 	import { _ } from 'svelte-i18n';
 	
-	const path: string = '/extensions/vault_manager';
+	const path: string = '/extensions/vault';
 	const description: string = 'Manage your vault balance and transfer tokens';
-	const title: string = SITE_NAME + ' - ' + $_('extensions.vault_manager.title');
-	const subtitle: string = $_('extensions.vault_manager.title');
+	const title: string = SITE_NAME + ' - ' + $_('extensions.vault.title');
+	const subtitle: string = $_('extensions.vault.title');
 </script>
 
 <MetaTag {path} {description} {title} {subtitle} />
